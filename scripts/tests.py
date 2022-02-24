131237a15bd49a2125431b64de4391391b98438d
Martin Budden <mjbudden@gmail.com>
2022-02-24 09:04:55 +0000

    Set image size in tests.py to 2048x2048.

--- scripts/tests.py
+++ scripts/tests.py
@@ -36,7 +36,7 @@ from blurb import *
 from colorama import Fore
 from tmpdir import *
 
-image_size = "--imgsize=4096,4096"
+image_size = "--imgsize=2048,2048"
 threshold = 20  # Image comparison allowed number of different pixels
 fuzz = 5       # Image comparison allowed percentage error in pixel value
 
